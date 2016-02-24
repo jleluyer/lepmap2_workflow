fdbfbfd4225485e4d7875347712d6aa37d33d80c
jeremyleluyer <jeremy.le.luyer@gmail.com>
2016-02-24 11:58:08 -0500

    changed scripts structure

--- 00-scripts/02_LepMap_SeparateChromosomes_template.sh
+++ 00-scripts/02_LepMap_SeparateChromosomes_template.sh
@@ -4,35 +4,31 @@
 #$ -m beas
 #$ -pe smp 1
 #$ -l h_vmem=20G
-#$ -l h_rt=00:20:00
+#$ -l h_rt=03:00:00
 #$ -cwd
 #$ -S /bin/bash
 
-#EstimateLod deprecated
-#java EstimateLODLimit data=/data4/sandrine/11_Lep_Map/data/Uneak_210_80_75_premakeped_file_20151002.linkage malePrior=-1
+#usage: java SeparateChromosomes [options] data=file [file2 file3...]
 
-INPUT="/home/jelel8/file_BEN_final_f"
+
+file="data=03-output/map_trimmed_f.linkage"
 DIR="/home/jelel8/Software/LepMap2/bin/"
-Mprior="0.01"
-infoM=0123
-#LOD=7
-#for LOD in $(cat list_LOD)
-java -cp $DIR SeparateChromosomes sizeLimit=22 data="$INPUT".linkage lodLimit="$LOD" malePrior="$Mprior" >map.lod"$LOD".NoinfoMask24.sizeLimit22.txt
+mp="malePrior=0.01"       		#Uniform prior for recombination fraction for (maternal) paternal haplotypes [0.05]
+#ap="(fe)malePrior=A B C"	     	#Affine prior for recombination fraction for (maternal) paternal haplotypes [not set]
+#t="(fe)maleTheta=NUM"		        #Use fixed recombination fraction [not set]
+#tml="(fe)maleTheta=ML"        		#Use maximum likelihood recombination fraction [not set]
+#R="removeMarkers=m1 [m2 ...]"		# Remove markers m1 m2 ... from further analysis [not set]
+im="informativeMask=23"     		#Use only markers with informative father (1), mother(2), both patrents(3) or neither parent(0) [0123]
+#f="families=f1 [f2 ...]"    		#Use only families f1 f2 ... [not set]
+sl="sizeLimit=1"
+#lod="lodLimit=12"
 
+#Testing various lod values
+for value in {1..30}
+do
+	lod=$(echo lodLimit="$value")
+java -cp $DIR SeparateChromosomes $file $sl $los $mp $ap $t $tml $R $im $f > 03-output/map_lod"$value"_sizeLimit$sl.txt
+done
 
-
-: '
-
-usage: java SeparateChromosomes [options] data=file [file2 file3...]
-options:
-         data=file [file2 ...]   Loads input genotypes in LINKAGE Pre-makeped format
-         lodLimit=NUM [NUM2 ...] LOD score limit [10.0]
-         (fe)malePrior=NUM       Uniform prior for recombination fraction for (maternal) paternal haplotypes [0.05]
-         (fe)malePrior=A B C     Affine prior for recombination fraction for (maternal) paternal haplotypes [not set]
-         (fe)maleTheta=NUM       Use fixed recombination fraction [not set]
-         (fe)maleTheta=ML        Use maximum likelihood recombination fraction [not set]
-         removeMarkers=m1 [m2 ...] Remove markers m1 m2 ... from further analysis [not set]
-         informativeMask=STR     Use only markers with informative father (1), mother(2), both patrents(3) or neither parent(0) [0123]
-         families=f1 [f2 ...]    Use only families f1 f2 ... [not set]
-         sizeLimit=NUM           Remove LGs with < NUM markers [1]
-'
+#optimal lod and sl
+#java -cp $DIR SeparateChromosomes $file $sl $los $mp $ap $t $tml $R $im $f > 03_output/map_lod"$value"_Mask23_sizeLimit1.txt
